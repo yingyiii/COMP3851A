9a3b39b735969f1fd70c7e49f4e2fb8f5b6b2192
yingyiii <127356681+yingyiii@users.noreply.github.com>
2023-06-29 00:15:23 +1000

    Update Planner.js

--- Planner.js
+++ Planner.js
@@ -1,204 +1,146 @@
-import React,{useState} from 'react';
-import './App.css'
-import Header from "./header";
-import Footer from "./footer";
+import React, { useState } from 'react';
+import './App.css';
+import Header from './header';
+import Footer from './footer';
 import Draggable from 'react-draggable';
 import { DragDropContext } from 'react-beautiful-dnd';
 import { render } from '@testing-library/react';
 
-const rowNames = [];
-
-const tableData1 = [
+const tableData = [
+  ['', '', '', ''],
+  ['', '', '', ''],
+  ['', '', '', ''],
+  ['', '', '', ''],
+  ['', '', '', ''],
   ['', '', '', ''],
 ];
-
-const tableData2 = [
-  ['', '', '', ''],
-];
-
-const tableData3 = [
-  ['', '', '', ''],
-];
-
-const tableData4 =[
-  ['', '', '', ''],
-];
-
-const tableData5 = [
-  ['', '', '', ''],
-];
-
-const tableData6 = [
-  ['', '', '', ''],
-];
-
-const handleDrag = (e, ui) => {
-  // Handle drag event
-};
-
-
 
 function Planner() {
+  const initialPositions = Array.from({ length: tableData.length }, () =>
+    Array.from({ length: 4 }, () => ({ x: 0, y: 0 }))
+  );
+  const [positions, setPositions] = useState(initialPositions);
+
+  const handleDrag = (sourceRowIndex, sourceCellIndex) => (e, ui) => {
+    const { x, y } = ui;
+    const newPositions = [...positions];
+    newPositions[sourceRowIndex][sourceCellIndex] = { x, y };
+    setPositions(newPositions);
+  };
+
+  const handleDragStop = (sourceRowIndex, sourceCellIndex) => () => {
+    const newPositions = [...positions];
+    newPositions[sourceRowIndex][sourceCellIndex] = { x: 0, y: 0 };
+    setPositions(newPositions);
+  };
+
+  const handleDragEnd = (result) => {
+    const { source, destination } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    const sourceRowIndex = parseInt(source.droppableId);
+    const sourceCellIndex = parseInt(source.index);
+    const destRowIndex = parseInt(destination.droppableId);
+    const destCellIndex = parseInt(destination.index);
+
+    if (sourceRowIndex === destRowIndex && sourceCellIndex === destCellIndex) {
+      return;
+    }
+
+    const draggedCell = tableData[sourceRowIndex][sourceCellIndex];
+    const updatedTableData = [...tableData];
+
+    // Swap the dragged cell with the destination cell
+    updatedTableData[sourceRowIndex][sourceCellIndex] = tableData[destRowIndex][destCellIndex];
+    updatedTableData[destRowIndex][destCellIndex] = draggedCell;
+
+    // Update the state
+    const newPositions = [...positions];
+    newPositions[sourceRowIndex][sourceCellIndex] = { x: 0, y: 0 };
+    newPositions[destRowIndex][destCellIndex] = { x: 0, y: 0 };
+    setPositions(newPositions);
+
+    render(<Planner />); // Force re-render to update the table with new positions and data
+  };
+
   return (
-      <div>
-          <Header />
-          <div style={{textAlign: "left", marginLeft:"30px"}}>
-            This program plan is for students comencing in the xxx major in Semester xx 2023.<n/>
-          </div>
-        <div class='row'>
-          <div class="column">
-        <table>
-        <tbody>
-        {tableData1.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            <th><span>SEMESTER 1 <br/> Year 1</span></th>
-            {row.map((cellData, cellIndex) => (
-              <td key={cellIndex}>
-                <Draggable
-                  handle=".handle"
-                  defaultPosition={{ x: 0, y: 0 }}
-                  onDrag={handleDrag}
-                >
-                  <div className="cell-container">
-                    <div className="handle">Drag me</div>
-                    <div className="cell-content">{cellData}</div>
-                  </div>
-                </Draggable>
-              </td>
-            ))}
-          </tr>
-          ))}
-          </tbody>
-        </table>
-
-        <table>
-        <tbody>
-        {tableData2.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            <th><span>SEMESTER 1 <br/>Year 2</span></th>
-            {row.map((cellData, cellIndex) => (
-              <td key={cellIndex}>
-                <Draggable
-                  handle=".handle"
-                  defaultPosition={{ x: 0, y: 0 }}
-                  onDrag={handleDrag}
-                >
-                  <div className="cell-container">
-                    <div className="handle">Drag me</div>
-                    <div className="cell-content">{cellData}</div>
-                  </div>
-                </Draggable>
-              </td>
-            ))}
-          </tr>
-          ))}
-          </tbody>
-        </table>
-
-        <table>
-        <tbody>
-        {tableData3.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            <th><span>SEMESTER 1<br/> Year 3 </span></th>
-            {row.map((cellData, cellIndex) => (
-              <td key={cellIndex}>
-                <Draggable
-                  handle=".handle"
-                  defaultPosition={{ x: 0, y: 0 }}
-                  onDrag={handleDrag}
-                >
-                  <div className="cell-container">
-                    <div className="handle">Drag me</div>
-                    <div className="cell-content">{cellData}</div>
-                  </div>
-                </Draggable>
-              </td>
-            ))}
-          </tr>
-          ))}
-          </tbody>
-        </table>
-        </div>
-
-        <div class="column">
-        <table>
-        <tbody>
-   
-        {tableData4.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            <th><span>SEMESTER 2</span></th>
-            {row.map((cellData, cellIndex) => (
-              <td key={cellIndex}>
-                <Draggable
-                  handle=".handle"
-                  defaultPosition={{ x: 0, y: 0 }}
-                  onDrag={handleDrag}
-                >
-                  <div className="cell-container">
-                    <div className="handle">Drag me</div>
-                    <div className="cell-content">{cellData}</div>
-                  </div>
-                </Draggable>
-              </td>
-            ))}
-          </tr>
-          ))}
-          </tbody>
-        </table>
-
-        <table>
-        <tbody>
-        {tableData5.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            <th><span>SEMESTER 2</span></th>
-            {row.map((cellData, cellIndex) => (
-              <td key={cellIndex}>
-                <Draggable
-                  handle=".handle"
-                  defaultPosition={{ x: 0, y: 0 }}
-                  onDrag={handleDrag}
-                >
-                  <div className="cell-container">
-                    <div className="handle">Drag me</div>
-                    <div className="cell-content">{cellData}</div>
-                  </div>
-                </Draggable>
-              </td>
-            ))}
-          </tr>
-          ))}
-          </tbody>
-        </table>
-
-        <table>
-        <tbody>
-        {tableData6.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            <th><span>SEMESTER 2</span></th>
-            {row.map((cellData, cellIndex) => (
-              <td key={cellIndex}>
-                <Draggable
-                  handle=".handle"
-                  defaultPosition={{ x: 0, y: 0 }}
-                  onDrag={handleDrag}
-                >
-                  <div className="cell-container">
-                    <div className="handle">Drag me</div>
-                    <div className="cell-content">{cellData}</div>
-                  </div>
-                </Draggable>
-              </td>
-            ))}
-          </tr>
-          ))}
-          </tbody>
-        </table>
-        </div>
-        </div>
-         
-          <Footer />
+    <div>
+      <Header />
+      <div style={{ textAlign: 'left', marginLeft: '30px' }}>
+        This program plan is for students commencing in the xxx major in Semester xx 2023.
+        <br />
       </div>
-  )
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <div className='row'>
+          <div className='column'>
+            {tableData.slice(0, 3).map((row, rowIndex) => (
+              <table key={rowIndex}>
+                <tbody>
+                  <tr>
+                    <th>
+                      <span>Year {rowIndex + 1}</span>
+                    </th>
+                    <th>
+                      <span>Semester 1</span>
+                    </th>
+                    {row.map((cellData, cellIndex) => (
+                      <td key={cellIndex}>
+                        <Draggable
+                          handle='.handle'
+                          defaultPosition={{ x: 0, y: 0 }}
+                          position={positions[rowIndex][cellIndex]}
+                          onDrag={handleDrag(rowIndex, cellIndex)}
+                          onStop={handleDragStop(rowIndex, cellIndex)}
+                        >
+                          <div className='cell-container'>
+                            <div className='handle'>Drag me</div>
+                            <div className='cell-content'>{cellData}</div>
+                          </div>
+                        </Draggable>
+                      </td>
+                    ))}
+                  </tr>
+                </tbody>
+              </table>
+            ))}
+          </div>
+          <div className='column'>
+            {tableData.slice(3).map((row, rowIndex) => (
+              <table key={rowIndex}>
+                <tbody>
+                  <tr>
+                    <th>
+                      <span>Semester 2</span>
+                    </th>
+                    {row.map((cellData, cellIndex) => (
+                      <td key={cellIndex}>
+                        <Draggable
+                          handle='.handle'
+                          defaultPosition={{ x: 0, y: 0 }}
+                          position={positions[rowIndex + 3][cellIndex]}
+                          onDrag={handleDrag(rowIndex + 3, cellIndex)}
+                          onStop={handleDragStop(rowIndex + 3, cellIndex)}
+                        >
+                          <div className='cell-container'>
+                            <div className='handle'>Drag me</div>
+                            <div className='cell-content'>{cellData}</div>
+                          </div>
+                        </Draggable>
+                      </td>
+                    ))}
+                  </tr>
+                </tbody>
+              </table>
+            ))}
+          </div>
+        </div>
+      </DragDropContext>
+      <Footer />
+    </div>
+  );
 }
 
 export default Planner;
