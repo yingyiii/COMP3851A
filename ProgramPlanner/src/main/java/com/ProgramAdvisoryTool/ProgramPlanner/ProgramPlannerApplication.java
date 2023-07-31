fbec631bdcade9183698263a29eadcfb4e163364
Mohammed Altamimi <81903280+MO56X@users.noreply.github.com>
2023-07-31 18:04:14 +1000

    Add files via upload

---
+++ ProgramPlanner/src/main/java/com/ProgramAdvisoryTool/ProgramPlanner/ProgramPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.ProgramAdvisoryTool.ProgramPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramPlannerApplication.class, args);
+	}
+
+}
---
+++ ProgramPlanner/src/test/java/com/ProgramAdvisoryTool/ProgramPlanner/ProgramPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProgramAdvisoryTool.ProgramPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
