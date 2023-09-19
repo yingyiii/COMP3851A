7b566c3c06fbd577b740cc28528187c5a532bf8b
yingyiii <127356681+yingyiii@users.noreply.github.com>
2023-09-19 13:22:07 +1000

    Add files via upload

--- App.js
+++ App.js
@@ -6,17 +6,18 @@ import StudentInfo from './StudentInfo.js';
 import './App.css';
 import Planner from './Planner.js';
 
+
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Routes>
-                    <Route path="/" element={<Home/>}/> 
-                    <Route path="/StudentInfo" element={<StudentInfo/>}/>
-                    <Route path='/Planner' element={<Planner/>}/>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/StudentInfo" element={<StudentInfo />} />
+                    <Route path='/Planner' element={<Planner />} />
                 </Routes>
+               
             </div>
-
             
         );
     }
--- Planner.js
+++ Planner.js
@@ -1,69 +1,95 @@
 import React, { useState } from 'react';
-import './App.css';
-import Header from './header';
+import Header from './header'; 
 import Footer from './footer';
-import Draggable from 'react-draggable';
-import { DragDropContext } from 'react-beautiful-dnd';
-import { render } from '@testing-library/react';
 
-const tableData = [
-  ['', '', '', ''],
-  ['', '', '', ''],
-  ['', '', '', ''],
-  ['', '', '', ''],
-  ['', '', '', ''],
-  ['', '', '', ''],
-];
-
-function Planner() {
-  const initialPositions = Array.from({ length: tableData.length }, () =>
-    Array.from({ length: 4 }, () => ({ x: 0, y: 0 }))
+const Box = ({ box, onDragStart, onDragOver, onDrop }) => {
+  return (
+    <div
+      className="box"
+      style={{ backgroundColor: box.color }}
+      draggable="true"
+      onDragStart={onDragStart({ id: box.id })}
+      onDragOver={onDragOver({ id: box.id })}
+      onDrop={onDrop({ id: box.id })}
+    >
+      <div className="content">{box.name}</div>
+    </div>
   );
-  const [positions, setPositions] = useState(initialPositions);
+};
 
-  const handleDrag = (sourceRowIndex, sourceCellIndex) => (e, ui) => {
-    const { x, y } = ui;
-    const newPositions = [...positions];
-    newPositions[sourceRowIndex][sourceCellIndex] = { x, y };
-    setPositions(newPositions);
+const BoxesGroup = () => {
+  const [boxes, setBoxes] = useState([
+    { id: 1, name: 'COMP3851A', color: 'lightgrey' },
+    { id: 2, name: 'COMP1010', color: 'lightgrey' },
+    { id: 3, name: 'SENG1050', color: 'lightgrey' },
+    { id: 4, name: 'COMP3500', color: 'lightgrey' },
+    { id: 5, name: 'SENG2260', color: 'lightgrey' },
+    { id: 6, name: 'SENG2130', color: 'lightgrey' },
+    { id: 7, name: 'COMP3851B', color: 'lightgrey' },
+    { id: 8, name: 'INFT1005', color: 'lightgrey' },
+    { id: 9, name: 'SENG1110', color: 'lightgrey' },
+    { id: 10, name: 'INFT1004', color: 'lightgrey' },
+    { id: 11, name: 'INFT1060', color: 'lightgrey' },
+    { id: 12, name: 'INFT3100', color: 'lightgrey' },
+    { id: 13, name: 'INFT3100', color: 'lightgrey' },
+    { id: 14, name: 'INFT3100', color: 'lightgrey' },
+    { id: 15, name: 'INFT3100', color: 'lightgrey' },
+    { id: 16, name: 'INFT3100', color: 'lightgrey' },
+    { id: 17, name: 'INFT3100', color: 'lightgrey' },
+    { id: 18, name: 'INFT3100', color: 'lightgrey' },
+    { id: 19, name: 'INFT3100', color: 'lightgrey' },
+    { id: 20, name: 'INFT3100', color: 'lightgrey' },
+    { id: 21, name: 'INFT3100', color: 'lightgrey' },
+    { id: 22, name: 'INFT3100', color: 'lightgrey' },
+    { id: 23, name: 'INFT3100', color: 'lightgrey' },
+    { id: 24, name: 'INFT3100', color: 'lightgrey' },
+  ]);
+
+  const leftBoxes = boxes.slice(0, 4);
+  const rightBoxes = boxes.slice(4, 8);
+
+  const swapBoxes = (fromBox, toBox) => {
+    const updatedBoxes = [...boxes];
+    const fromIndex = boxes.findIndex((box) => box.id === fromBox.id);
+    const toIndex = boxes.findIndex((box) => box.id === toBox.id);
+
+    if (fromIndex !== -1 && toIndex !== -1) {
+      [updatedBoxes[fromIndex], updatedBoxes[toIndex]] = [
+        updatedBoxes[toIndex],
+        updatedBoxes[fromIndex],
+      ];
+      setBoxes(updatedBoxes);
+    }
   };
 
-  const handleDragStop = (sourceRowIndex, sourceCellIndex) => () => {
-    const newPositions = [...positions];
-    newPositions[sourceRowIndex][sourceCellIndex] = { x: 0, y: 0 };
-    setPositions(newPositions);
+  const handleDragStart = (data) => (event) => {
+    const fromBox = JSON.stringify({ id: data.id });
+    event.dataTransfer.setData('dragContent', fromBox);
   };
 
-  const handleDragEnd = (result) => {
-    const { source, destination } = result;
+  const handleDragOver = (data) => (event) => {
+    event.preventDefault();
+  };
 
-    if (!destination) {
-      return;
-    }
+  const handleDrop = (data) => (event) => {
+    event.preventDefault();
 
-    const sourceRowIndex = parseInt(source.droppableId);
-    const sourceCellIndex = parseInt(source.index);
-    const destRowIndex = parseInt(destination.droppableId);
-    const destCellIndex = parseInt(destination.index);
+    const fromBox = JSON.parse(event.dataTransfer.getData('dragContent'));
+    const toBox = { id: data.id };
 
-    if (sourceRowIndex === destRowIndex && sourceCellIndex === destCellIndex) {
-      return;
-    }
+    swapBoxes(fromBox, toBox);
+  };
 
-    const draggedCell = tableData[sourceRowIndex][sourceCellIndex];
-    const updatedTableData = [...tableData];
-
-    // Swap the dragged cell with the destination cell
-    updatedTableData[sourceRowIndex][sourceCellIndex] = tableData[destRowIndex][destCellIndex];
-    updatedTableData[destRowIndex][destCellIndex] = draggedCell;
-
-    // Update the state
-    const newPositions = [...positions];
-    newPositions[sourceRowIndex][sourceCellIndex] = { x: 0, y: 0 };
-    newPositions[destRowIndex][destCellIndex] = { x: 0, y: 0 };
-    setPositions(newPositions);
-
-    render(<Planner />); // Force re-render to update the table with new positions and data
+  const makeBoxes = () => {
+    return boxes.map((box) => (
+      <Box
+        key={box.id}
+        box={box}
+        onDragStart={handleDragStart}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      />
+    ));
   };
 
   return (
@@ -72,75 +98,35 @@ function Planner() {
       <div style={{ textAlign: 'left', marginLeft: '30px' }}>
         This program plan is for students commencing in the xxx major in Semester xx 2023.
         <br />
+        <br />
       </div>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <div className='row'>
-          <div className='column'>
-            {tableData.slice(0, 3).map((row, rowIndex) => (
-              <table key={rowIndex}>
-                <tbody>
-                  <tr>
-                    <th>
-                      <span>Year {rowIndex + 1}</span>
-                    </th>
-                    <th>
-                      <span>Semester 1</span>
-                    </th>
-                    {row.map((cellData, cellIndex) => (
-                      <td key={cellIndex}>
-                        <Draggable
-                          handle='.handle'
-                          defaultPosition={{ x: 0, y: 0 }}
-                          position={positions[rowIndex][cellIndex]}
-                          onDrag={handleDrag(rowIndex, cellIndex)}
-                          onStop={handleDragStop(rowIndex, cellIndex)}
-                        >
-                          <div className='cell-container'>
-                            <div className='handle'>Drag me</div>
-                            <div className='cell-content'>{cellData}</div>
-                          </div>
-                        </Draggable>
-                      </td>
-                    ))}
-                  </tr>
-                </tbody>
-              </table>
-            ))}
-          </div>
-          <div className='column'>
-            {tableData.slice(3).map((row, rowIndex) => (
-              <table key={rowIndex}>
-                <tbody>
-                  <tr>
-                    <th>
-                      <span>Semester 2</span>
-                    </th>
-                    {row.map((cellData, cellIndex) => (
-                      <td key={cellIndex}>
-                        <Draggable
-                          handle='.handle'
-                          defaultPosition={{ x: 0, y: 0 }}
-                          position={positions[rowIndex + 3][cellIndex]}
-                          onDrag={handleDrag(rowIndex + 3, cellIndex)}
-                          onStop={handleDragStop(rowIndex + 3, cellIndex)}
-                        >
-                          <div className='cell-container'>
-                            <div className='handle'>Drag me</div>
-                            <div className='cell-content'>{cellData}</div>
-                          </div>
-                        </Draggable>
-                      </td>
-                    ))}
-                  </tr>
-                </tbody>
-              </table>
-            ))}
-          </div>
-        </div>
-      </DragDropContext>
+      
+        <tbody>
+          <tr>
+            <th>
+              <span>YEAR 1</span>
+              <span>YEAR 2</span>
+              <span>YEAR 3</span><br/>
+            </th>
+            <td>
+            <div className="semester-container">
+            <div className="semester-labels">
+            <div>
+              <span className="left-semester">SEMESTER 1</span>
+              <span className="right-semester">SEMESTER 2</span>
+            </div>
+            </div>
+        
+      </div>
+            <div className="boxesGroup">{makeBoxes()} </div> 
+            </td>
+          </tr>
+        </tbody>
+
       <Footer />
     </div>
   );
-}
+  
+};
 
-export default Planner;
+export default BoxesGroup;
